7e43056c15c2698e7ef092225f9272946447981f
beatrizrckr97 <152621606+beatrizrckr97@users.noreply.github.com>
2025-06-15 16:39:59 -0300

    Revert "feat: atualiza models, admin e formulários do projeto integrador"
    This reverts commit b3dc9373dd8f0047891230fc1223acc524ca73ad.

--- prefeitura-proxima-app-main/core/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Servico, RequisicaoServico
-
-admin.site.register(Servico)
-admin.site.register(RequisicaoServico)
--- prefeitura-proxima-app-main/core/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import RequisicaoServico
-
-class RequisicaoServicoForm(forms.ModelForm):
-    class Meta:
-        model = RequisicaoServico
-        fields = ['nome_cidadao', 'email', 'servico', 'descricao_requisicao']
